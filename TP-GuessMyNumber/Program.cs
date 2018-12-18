91b37bbf490004f49c311b0019fda4869248bbb2
Sulyvan Debraize <sulyvan.debraize@net-c.com>
2018-12-18 17:08:27 +0100

    TP Terminé

---
+++ TP-GuessMyNumber/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_GuessMyNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer;
+            int attempts = 0;
+            int attempt = 0;
+
+            Console.WriteLine("Entrez le numéro à faire deviner à l'autre joueur.");
+            Console.WriteLine("Il doit être compris entre 1 et 50. (0 = Aléatoire)");
+            while (true)
+            {
+                answer = int.Parse(Console.ReadLine());
+                if (answer > 0 && answer < 51)
+                {
+                    Console.WriteLine("Votre choix est fait. Appuyez sur n'importe quelle touche pour continuer.");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                }
+                else
+                {
+                    if (answer == 0)
+                    {
+                        Random randomizer = new Random();
+                        answer = randomizer.Next(1, 51);
+                        Console.WriteLine("Votre choix est fait, le nombre sera généré aléatoirement. "+Environment.NewLine+" Appuyez sur n'importe quelle touche pour continuer.");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    }
+
+                    Console.WriteLine("On à dit entre 1 et 50 !!");
+                }
+            }
+
+            while (attempt != answer)
+            {
+                Console.WriteLine("Trouvez le bon numéro entre 1 et 50 !");
+                attempt = int.Parse(Console.ReadLine());
+                attempts++;
+            }
+
+            Console.WriteLine($"Félicitations vous avez trouvé le numéro au bout de { attempts } essais !");
+        }
+    }
+}
